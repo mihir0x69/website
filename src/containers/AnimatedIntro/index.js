124ce1aafa88222b00886e188f9c72c16cc34351
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-06-16 12:15:05 +0530

    Move SCs to fragments in AnimatedIntro

--- src/containers/AnimatedIntro/index.js
+++ src/containers/AnimatedIntro/index.js
@@ -1,75 +1,10 @@
 import React, { useEffect } from 'react'
-import styled from 'styled-components'
 import ProgressiveImage from 'react-progressive-image'
-import { Link } from 'react-router-dom'
 import Paths from 'constants/paths'
+import { Wrapper, Images, Glasses, StartButton } from './fragments'
 
 const SPACEBAR = 32
 const ENTER = 13
-
-const Wrapper = styled.div`
-    font-family: ${props => props.theme.fonts.retro};
-    color: ${props => props.theme.colors.foreground};
-    height: 100vh;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-`
-
-const Images = styled.div`
-    position: relative;
-    overflow: hidden;
-`
-
-const Glasses = styled.img.attrs({
-    alt: 'Glasses',
-})`
-    width: 60px;
-    position: absolute;
-    top: 48px;
-    right: 68px;
-    animation: thugmove 4s;
-
-    @-webkit-keyframes thugmove {
-        from {
-            top: -20px;
-        }
-        to {
-            top: 48px;
-        }
-    }
-
-    @keyframes thugmove {
-        from {
-            top: -20px;
-        }
-        to {
-            top: 48px;
-        }
-    }
-`
-
-const StartButton = styled(Link)`
-    color: ${({ theme }) => theme.colors.foreground};
-    text-decoration: none;
-    animation: blink-animation 1s steps(5, start) infinite;
-    -webkit-animation: blink-animation 1s steps(5, start) infinite;
-    animation-delay: 3s;
-
-    @keyframes blink-animation {
-        to {
-            visibility: hidden;
-        }
-    }
-    @-webkit-keyframes blink-animation {
-        to {
-            visibility: hidden;
-        }
-    }
-`
 
 const Content = ({ heroes, onPressStart }) => (
     <React.Fragment>
