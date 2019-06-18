611793e33d0f25e66fead25232aad2a52f6c06ca
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-06-18 22:18:44 +0530

    Initial mardown usage

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import marked from 'marked'
 
-const Blog = (props) => <div>{JSON.stringify(props)}</div>
+const markup = marked(`---
+# h1 Heading 8-)
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading`
+)
+
+const Blog = (props) => 
+    <div dangerouslySetInnerHTML={{ __html: markup }}>
+    </div>
 
 export default Blog
