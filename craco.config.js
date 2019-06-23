d0c4be7aecae24c1a7944b1c1389423715112c86
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-06-23 14:48:30 +0530

    Add config for prism and raw loader (md)

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,27 @@
+const _ = require('lodash')
+
 module.exports = {
     babel: {
-        "plugins": ["babel-plugin-styled-components"]
+        "plugins": [
+            "babel-plugin-styled-components",
+            ["babel-plugin-prismjs", {
+                "languages": ["javascript", "css", "markup"],
+                "plugins": ["line-numbers"],
+                "theme": "default",
+                "css": true
+            }]
+        ]
     },
     webpack: {
         configure: (config) => {
             config.resolve.modules.push('src')
+            const rules = _.find(config.module.rules, x => x.oneOf)
+            const fileLoaderRuleIndex = _.findIndex(
+                rules, x => _.includes(x.loader, 'file-loader'))
+            rules.oneOf.splice(fileLoaderRuleIndex, 0, {
+                test: /\.md$/,
+                loader: 'raw-loader'
+            })
             return config
         }
     }
