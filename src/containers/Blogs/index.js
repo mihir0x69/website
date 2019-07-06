272900220eccfe78bcfc221b3b17aed8433b44de
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-07-06 12:34:49 +0530

    Update loading screen

--- src/containers/Blogs/index.js
+++ src/containers/Blogs/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { Redirect } from 'react-router-dom'
 import BlogTemplate from 'components/common/BlogTemplate'
 import paths from 'constants/paths'
+import loaderImage from 'media/LanceWalkingR.gif'
 
 const getContents = async id => {
     const { default: metadata } = await import(`./${id}/metadata.json`)
@@ -12,6 +14,25 @@ const getContents = async id => {
         content,
     }
 }
+
+const Loading = styled.div`
+    font-family: ${props => props.theme.fonts.retro};
+    position: fixed;
+    top: 100px;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const LanceWalking = styled.img.attrs({ 
+    src: loaderImage,
+    alt: 'Loading'
+})`
+    margin: 0 20px;
+`
 
 const Blog = props => {
     const [data, setData] = useState(null)
@@ -27,7 +48,12 @@ const Blog = props => {
     }
 
     if (!data) {
-        return 'Loading...'
+        return (
+            <Loading>
+                <LanceWalking/>
+                {'Loading...'}
+            </Loading>
+        )
     }
 
     return <BlogTemplate {...data} />
