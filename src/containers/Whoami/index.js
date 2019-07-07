a0716529701ee832d62aab8377ceeadd7522cd39
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-07-07 13:13:03 +0530

    Add quotes from friends

--- src/containers/Whoami/index.js
+++ src/containers/Whoami/index.js
@@ -1,16 +1,62 @@
 import React from 'react'
 import styled from 'styled-components'
+import hero from 'media/mihir.jpg'
+import quotes from './quotes'
 
 const Title = styled.h2`
     font-family: ${props => props.theme.fonts.retro};
+    text-align: center;
 `
 
 const Whoami = () => {
     return (
-        <div>
-            <Title>{'whoami'}</Title>
-            <p>{'Regular dude from India'}</p>
-        </div>
+        <>
+            <div style={{ textAlign: 'center' }}>
+                <Title>{'whoami'}</Title>
+                <p><i>{"*Interstellar Main Theme plays in the background*"}</i></p>
+                <img src={hero} alt="hero" style={{ width: '100%' }} />
+                <p>
+                    {`My name is Mihir. I'm a software developer from Pune, India. I write Javascript,
+                    C# and F#. I enjoy working on Microsoft Azure. Working on cool stuff at my day job.
+                    I love building things from scratch. I enjoy watching small things come together
+                    and form a huge system. I loathe unnecessary abstraction and overengineering. I strive
+                    to keep things simple.
+                    `}
+                </p>
+                <p>
+                    {`I'm a cat lover. Dogs are okay too. I love travelling. I love observing people - 
+                    their culture, their way of living. Planning on travelling the world. But
+                    duties take precedence ATM.
+                    `}
+                </p>
+                <p>
+                    {`I like to imagine myself as an intriguing human being. I can talk about `}
+                    <a href="https://en.wikipedia.org/wiki/Advaita_Vedanta">{'Advait Vedanta'}</a>
+                    {` and `}
+                    <a href="https://en.wikipedia.org/wiki/Rakhi_Sawant">{'Rakhi Sawant'}</a>
+                    {` in the same conversation and still not be overwhelmed. I think I have 
+                    figured out my life - Just working out a few kinks. Hit me up on `}
+                    <a href="https://twitter.com/KarandikarMihir">{'Twitter'}</a>
+                    {' if you want to talk.'}
+                </p>
+                <p>
+                    {`My family and my friends are everything to me. I'd give away everything just to be
+                    with them for a little longer in this realm. Let's have them say something about me -`}
+                </p>
+            </div>
+            {
+                quotes.map((q, idx) => (
+                    <blockquote key={idx}>
+                        {q.text}
+                        <br />
+                        {`- ${q.author}`}
+                    </blockquote>
+                ))
+            }
+            <div style={{ textAlign: 'center' }}>
+                <p><b>{`God damn it guys!! I'm trying really hard to be nice to people.`}</b></p>
+            </div>
+        </>
     )
 }
 
---
+++ src/containers/Whoami/quotes.js
@@ -0,0 +1,32 @@
+const quotes = [
+    { 
+        text: 'I can crack dirtiest of jokes with you and I feel very comfortable talking to you. You are just like me 😎. Stop spending so much of your time on your phone.',
+        author: 'Aditya'
+    },
+    {
+        text: 'You stick with your people no matter what happens or will happen. But you waste your talent by not wanting to come out of your comfort zone.',
+        author: 'Atharva'
+    },
+    {
+        text: 'You are really smart. You are very opinionated. You can be more open minded.',
+        author: 'Dhanno'
+    },
+    {
+        text: "Be more friendly to people, share more things. And don't be so reserved, *Marathi curse words*.",
+        author: 'Jitesh'
+    },
+    {
+        text: 'You are loyal to the bone. But rigid af.',
+        author: 'Kaushik'
+    },
+    {
+        text: 'You are a loyal friend who cares deeply. But you overthink things and are a bad communicator when it comes to your own problems.',
+        author: 'Nikita'
+    },
+    {
+        text: "Your Kokanastha essence kicks in when you feel rightful to be mean to people but they can't be mean to you.",
+        author: 'Sonal'
+    }
+]
+
+export default quotes
