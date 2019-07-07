c38d11b15443dfb750c66a3f9dc51bd79181ece5
Mihir Karandikar <karandikar.mihir@outlook.com>
2019-07-07 09:11:17 +0530

    Put all routes into an array and map over them

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,20 @@ import Places from 'containers/Places'
 import Whoami from 'containers/Whoami'
 import Layout from 'components/common/Layout'
 
+const routes = [
+    { path: paths.ROOT, exact: true, component: AnimatedIntro },
+    { path: paths.MENU, component: Menu },
+    { path: paths.BLOGS_PATH, component: BlogList },
+    { path: paths.BLOG, component: Blog },
+    { path: paths.PLACES, component: Places },
+    { path: paths.WHOAMI, component: Whoami }
+]
+
 const Routes = () => {
     return (
         <Router>
             <Layout>
-                <Route path={paths.ROOT} exact component={AnimatedIntro} />
-                <Route path={paths.MENU} component={Menu} />
-                <Route path={paths.BLOGS_PATH} component={BlogList} />
-                <Route path={paths.BLOG} component={Blog} />
-                <Route path={paths.PLACES} component={Places} />
-                <Route path={paths.WHOAMI} component={Whoami} />
+                {routes.map(props => <Route {...props} />)}
             </Layout>
         </Router>
     )
